57d5c6933181d422e2231c7871656d8e53f32919
Fernando <fernando.lopes48@etec.sp.gov.br>
2025-09-03 14:56:14 -0300

    adição de php e formulario

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <p>&copy; 2024 Madruga's Barber. Todos os direitos reservados.</p>
+    </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Madruga's Barber</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="imagens/logo.svg" alt="Madruga's Barber Logo" class="logo">
+        </div>
+        <nav>
+            <a href="#sobre-nos">Sobre Nós</a>
+            <a href="#contato">Contate-nos</a>
+        </nav>
+    </header>
